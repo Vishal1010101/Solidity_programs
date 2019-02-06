14d8842ef06318fbcd012efdf3daef3b89dfefc1
Vishal1010101 <vishalcodebrew@gmail.com>
2019-02-06 17:48:39 +0530

    6 feb 5:50

--- erc20_token.sol
+++ erc20_token.sol
@@ -1,45 +1,43 @@
 pragma solidity ^0.5.1;
 
 contract ERC20Interface {
-    
-    function totalSupply() public view returns (uint256);
+    uint256 public totalSupply; 
     function balanceOf(address _owner) public view returns (uint256 balance);
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
     function transfer(address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+    
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _tokenOwner, address indexed _spender, uint _value);
 }
 
 
 contract MyToken is ERC20Interface{
-    
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
     string public name;           // name of the token 
     string public symbol;         // symbol of the token
     uint256 public decimals;         // conversions from wei to ether 
-    uint256 private totalSupplies;    // total token issued
     
-    uint256 private MAX_UINT256 = 2**256 - 1;
     mapping (address => uint256) public balances;
     mapping (address => mapping (address => uint256)) public allowed;
     
-    constructor(uint256 initialSupply, string memory _name,string memory _symbol, uint256 _decimals) 
-    public{
-        balances[msg.sender] == initialSupply;
-        totalSupplies = initialSupply;
+    constructor(uint256 _initialSupply, string memory _name,string memory _symbol, uint256 _decimals) public{
+        balances[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
     }
     
-    function totalSupply() public view returns (uint256) {
-        return totalSupplies;
+       function balanceOf(address _owner) public view returns (uint256 balance){
+        return balances[_owner];
     }
     
     function transfer(address _to, uint256 _value) public returns(bool success){
         require(balances[msg.sender] >= _value, "the balance should be more than what we send to the sender");
+        require(balances[_to] + _value >= balances[_to]);
         balances[msg.sender] -= _value;
         balances[_to] += _value;
         emit Transfer(msg.sender , _to, _value);
@@ -58,12 +56,8 @@ contract MyToken is ERC20Interface{
         return true;
     }
     
-    function balanceOf(address _owner) public view returns (uint256 balance){
-        return balances[_owner];
-    }
-    
     function approve(address _spender, uint256 _value) public returns (bool success){
-        allowed[_spender][msg.sender] = _value;
+        allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }  
