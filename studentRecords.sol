df26dcb51072f01d1baed46f201891408ee58026
Vishal1010101 <vishalcodebrew@gmail.com>
2019-02-27 11:48:15 +0530

    count added

--- studentRecords.sol
+++ studentRecords.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.5.1;
 contract StudentRecord{
     
     address internal owner;
+    uint internal count = 0;
     
-    constructor() internal onlyOwner{
+    constructor() public{
         owner = msg.sender;
     }
     
@@ -13,30 +14,35 @@ contract StudentRecord{
         uint age;                   
         string courses;          
         uint year;            
-        address rollno;       
+        uint rollno;       
     }
     
     Student[] internal allStudents;
 
-    function createStudentRecords(string memory _stuName, uint _stuAge,string memory _stuCourses, uint _stuYear)  public onlyOwner{
+    function createStudentRecords(string memory _stuName, uint _stuAge,string memory _stuCourses, uint _stuYear, uint _stuRollNo)  public onlyOwner{
         Student memory newStudent = Student({
             name: _stuName,
             age: _stuAge,
             courses : _stuCourses, 
             year : _stuYear,
-            rollno : msg.sender
+            rollno : _stuRollNo
         });
         allStudents.push(newStudent);
+        allStudents[count] = newStudent;
+        count++;
     }
     
-    function displayRecords(uint index) public view returns(string memory, string memory, uint, address){
-        require(index >=0);
+    function displayRecords(uint index) public view returns(string memory, string memory, uint, uint){
+        require(index >=0 && index<= count);
         return(allStudents[index].name, allStudents[index].courses, allStudents[index].age, allStudents[index].rollno);
+    }
+    
+    function displayTotalStudentsCount() public view returns(uint){
+        return count;
     }
     
     modifier onlyOwner(){
         require(owner == msg.sender);
         _;
     }
-
 }
